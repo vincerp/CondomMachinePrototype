c2232d60494dbc41c80c40e7d280caf45322657c
vincerp <vincependrell@gmail.com>
2014-05-01 19:30:54 +0200

    go go go

---
+++ unity/Assets/Scripts/Editor/Label3DDrawer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Label3D))]
+public class Label3DDrawer : Editor {
+
+	[DrawGizmo(GizmoType.NotSelected|GizmoType.SelectedOrChild)]
+	static void DrawSomething(Transform objectTransform, GizmoType gizmoType){
+		Label3D l3d = objectTransform.GetComponent<Label3D>();
+
+		if(null == l3d) return;
+
+		Handles.Label(((l3d.target)?l3d.target.position:objectTransform.position) + l3d.offset, l3d.text, "Button");
+
+	}
+
+//	void OnSceneGUI(){
+//		MonoBehaviour mb = target as MonoBehaviour;
+//		Transform tr = mb.transform;
+//
+//		Handles.Label(tr.position, "Whatever");
+//	}
+}
---
+++ unity/Assets/Scripts/Label3D.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Label3D : MonoBehaviour {
+
+	public string text;
+
+	public Vector3 offset = Vector3.zero;
+
+	public Transform target;
+}
